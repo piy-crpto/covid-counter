e2eace666979b17e91689301e7d4cbc27d7ca4ee
Piyush Yadav <piyushyadav492@gmail.com>
2022-10-05 00:06:05 +0530

    first commit

---
+++ src/Components/Statewise.js
@@ -0,0 +1,73 @@
+import React ,{useEffect,useState} from 'react'
+import './Statewise.css';
+
+
+const Statewise = () =>
+
+{         const[data,setData] = useState([]);
+    const getCovidData = async() =>
+    {
+      const res =   await fetch('http:/api.covid19india.org/data.json');
+      const actualData = await res.json();
+      setData(actualData.statewise);
+     // console.log(actualData);
+    }
+    useEffect(() => {
+       getCovidData();
+    
+    },[]);
+    
+   return 
+
+  
+   (
+       <>
+     
+        <div className="container-fluid mt-5">
+                    <div className="main-heading">
+                        <h1 className="mb-5 text-center"><span className='font-weight-bold'> INDIA </span>COVID-19 Dashboard</h1>
+                    </div>
+                    <div className="table-responsive">
+                            <table className="table table-hover">
+                                <thead className='thead-dark'>
+                                    <tr>
+                                        <th>State</th>
+                                        <th>Confirmed</th>
+                                        <th>Recovered</th>
+                                        <th>Deaths</th>
+                                        <th>Active</th>
+                                        <th>Updated</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                 
+                                 {
+                                     data.map((currElem,indx) =>
+                                     {
+                                        return (
+                                            <tr key={indx}>
+                                            <th>{currElem.state}</th>
+                                            <th>{currElem.confirmed}</th>
+                                            <th>{currElem.recovered}</th>
+                                            <th>{currElem.deaths}</th>
+                                            <th>{currElem.active}</th>
+                                            <th>{currElem.lastupdatedtime}</th>
+                                        </tr>
+                                        )
+                                     })
+                                 }
+
+
+                             
+                                </tbody>
+                            </table>
+                    </div>
+
+       </div>
+
+   </>
+   )
+};
+
+export default Statewise;
